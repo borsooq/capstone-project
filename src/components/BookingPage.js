b35fd39ee230a62f460384a2022eee4f9b1755d5
Mirko Boruc <borsooq@gmail.com>
2023-11-02 19:20:33 +0100

    Added Booking Form with very basic structure

--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./../App.css";
+import BookingForm from "./BookingForm";
 
 export default function BookingPage() {
-  return <>BookingPage</>;
+  return (
+    <>
+      <h2>Booking Page</h2>
+      <BookingForm />
+    </>
+  );
 }
